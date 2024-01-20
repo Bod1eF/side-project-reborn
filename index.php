0069ca6a8f10fa6cff43a5ff4b64f9b3f4b73984
Bodie Feinberg <bodiefeinberg123@gmail.com>
2024-01-20 14:22:48 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+<title>Side Project</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+<link href="index.css" rel="stylesheet" type="text/css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+</body>
+</html>
