b1cd356ce1ce260640b007d7c1b8de0f23fe541a
Bodie Feinberg <bodiefeinberg123@gmail.com>
2024-01-20 14:27:32 -0800

    start

--- index.php
+++ index.php
@@ -1,23 +1,16 @@
 <!doctype html>
 <html lang="en">
 <head>
-<title>Cul Cavboj</title>
+ <title>Side Project</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width">
-<link href="cssandjs/home.css" rel="stylesheet" type="text/css" />
+<link href="index.css" rel="stylesheet" type="text/css" />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="cssandjs/home.js"></script>
 </head>
 <body>
-  <iframe src="audio/silence.mp3" allow="autoplay" id="audio"></iframe>
-
-  <audio autoplay>
-    <source src="audio/main_theme.mp3" type="audio/mp3">
-  </audio>
-
-<a href="login.php">
-<img id="cul_home" src="img/home_bg.png" alt="Cul Cavboj background)" />
-</a>
+  <h1> Side Project </h1>
+  <a href="signin.php">Sign In</a>
 
 </body>
 </html>
