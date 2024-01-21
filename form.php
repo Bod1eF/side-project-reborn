f552c502cb9483aaadb524a4dfa10e63d8138abe
Bodie Feinberg <bodiefeinberg123@gmail.com>
2024-01-20 22:25:14 -0800

    made form legit

--- form.php
+++ form.php
@@ -1,3 +1,32 @@
+<?php
+  session_start();
+  require_once "sql_config.php";
+
+  try {
+
+    if (isset($_POST["user_login"]) && isset($_POST["pass_login"])) {//checks that user came from login
+      var_dump($_POST);}
+      $dbh =  new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+      $sth_password = $dbh->prepare("SELECT * FROM user WHERE email=:login_email");//find pass_hash where id matches login id
+      $sth_password->bindValue(':login_email', htmlspecialchars($_POST["user_login"]));
+      $sth_password->execute();
+      $login_row = $sth_password->fetch();
+      $user_id = $login_row["id"]; 
+      $_SESSION["user_id"] = $user_id; //store user id in session
+
+    if (password_verify(htmlspecialchars($_POST["pass_login"]), $login_row['password'])) { //verify password agaisnt hash
+        echo "<h1 id='logged_in'> Succesfully Logged in as " . htmlspecialchars($_POST["user_login"]) . "</h2>";
+    }
+    else { //if password doesn't match send to sign in
+      header('Location: index.php');
+      exit;
+    }
+  }
+
+  catch (PDOException $e) {
+    echo "<p>Error: {$e->getMessage()}</p>";
+    }
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -92,7 +121,7 @@ body {font-size:16px;}
 
         <div class="w3-section">
           <label><p style="color:white">Message</p></label>
-          <textarea id="area" rows="4" cols="100" maxlength="300" placeholder="Enter your Text Here"> 
+          <textarea id="area" rows="4" cols="100" maxlength="300" name="body" placeholder="Enter your Text Here"> 
           </textarea>
           <p class="result" style="color:white"> 
             <span id="word" style="color:white">0</span> Words and
